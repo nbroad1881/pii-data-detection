0de40ff4a967612d97cc81a2719bb707b0ef2437
Nicholas Broad <nicholas@nmbroad.com>
2024-03-30 23:16:06 -0700

    v 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='piidd',
-    version='0.1',
+    version='0.1.6',
     packages=find_packages(),
     author='Nicholas Broad',
     description='Code for PII data detection Kaggle competition',
